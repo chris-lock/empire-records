9d95812963adbc59abd67d1dc061d0e53d872f8b
Chris Lock <lock.christopher@gmail.com>
2014-11-03 02:08:28 -0500

    Adding values so class properties are instantiated.

--- src/command_line_tool.rb
+++ src/command_line_tool.rb
@@ -3,8 +3,8 @@ require_relative 'command_line_utility'
 require 'optparse'
 
 class CommandLineTool < CommandLineUtility
-	@@command_format
-	@executing_file_name
+	@@command_format = ''
+	@executing_file_name =''
 
 	def initialize(current_file)
 		@executing_file_name = File.basename(current_file)
@@ -26,12 +26,24 @@ class CommandLineTool < CommandLineUtility
 	def run(); end
 
 	def raise_arguments_error(arguments_arity, arguments_given)
-		raise_error(
+		raise_usage_error(
 			"#{bold(red(@executing_file_name))} takes " +
 			"#{bold(red(arguments_arity))} #{pluralize('arguement', arguments_arity)}. " +
-			"#{bold(red(arguments_given))} given.\n" +
-			"#{@executing_file_name} #{@@command_format}"
+			"#{bold(red(arguments_given))} given."
 		)
+	end
+
+	def raise_usage_error(error)
+		raise_error(get_usage_error_message(error))
+	end
+
+	def get_usage_error_message(error)
+		return "#{error}\n" +
+			"#{@executing_file_name} #{@@command_format}"
+	end
+
+	def raise_fatal_usage_error(error)
+		raise_fatal_error(get_usage_error_message(error))
 	end
 
 	def pluralize(string, count)
